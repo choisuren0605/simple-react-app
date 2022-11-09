cf05d8229ef0a740165f1a2ac6fceda1d7e42b6e
Choisuren <choisurena@gmail.com>
2022-11-09 13:40:16 +0800

    add Sidebar

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -13,15 +13,16 @@ const Sidebar = () => {
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="25"
-              height="25"
-              stroke-width="2.5"
-              viewBox="0 0 24 24"
+              fill="none"
+              width="28"
+              height="28"
+              viewBox="0 0 30 30"
+              strokeWidth="2.5"
               stroke="#1478DD"
-              fill="currentColor"
-              class="bi bi-bag"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             >
-              <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+              <path d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
             </svg>
 
             <span>Захиалга өгөх</span>
@@ -36,10 +37,9 @@ const Sidebar = () => {
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              class="icon icon-tabler icon-tabler-login"
-              width="25"
-              height="25"
-              viewBox="0 0 24 24"
+              width="28"
+              height="28"
+              viewBox="0 0 30 30"
               stroke-width="2.5"
               stroke="#1478DD"
               fill="none"
